e509d70bddb9c6085f243295487687bf91706819
alexandre555 <alexandremsantos555@gmail.com>
2022-03-08 22:19:43 -0300

    CRUD Scaffolding

---
+++ Projeto ASPNET/Data/Projeto_ASPNETContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Projeto_ASPNET.Models;
+
+namespace Projeto_ASPNET.Data
+{
+    public class Projeto_ASPNETContext : DbContext
+    {
+        public Projeto_ASPNETContext (DbContextOptions<Projeto_ASPNETContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Projeto_ASPNET.Models.Department> Department { get; set; }
+    }
+}
--- Projeto ASPNET/Startup.cs
+++ Projeto ASPNET/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Projeto_ASPNET.Data;
 
 namespace Projeto_ASPNET
 {
@@ -24,6 +26,9 @@ namespace Projeto_ASPNET
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<Projeto_ASPNETContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("Projeto_ASPNETContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
